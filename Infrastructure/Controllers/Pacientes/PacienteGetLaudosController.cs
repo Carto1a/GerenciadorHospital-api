4175755efa8a553c756ee1f1e10d691e26fdcaa1
Cartola <49589600+Carto1a@users.noreply.github.com>
2024-06-19 00:41:31 -0300

    refactor: more paciente get endpoint

---
+++ Infrastructure/Controllers/Pacientes/PacienteGetLaudosController.cs
@@ -0,0 +1,42 @@
+using Hospital.Application.Dto.Input.Atendimentos;
+using Hospital.Application.Dto.Input.Laudos;
+using Hospital.Application.UseCases.Atendimentos;
+using Hospital.Application.UseCases.Laudos;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Infrastructure.Controllers.Pacientes;
+[ApiController]
+[Route("api/Paciente")]
+[Tags("Paciente")]
+public class PacienteGetLaudosController : ControllerBase
+{
+    [HttpGet("Laudos")]
+    [Authorize(Roles = "PACIENTE")]
+    public async Task<IActionResult> Execute(
+        [FromServices] LaudoGetByQueryUseCase _service,
+        [FromQuery] LaudoGetByQueryDto query)
+    {
+        var id = User.Claims.FirstOrDefault(c => c.Type == "ID")?.Value;
+        if (id == null)
+            return Unauthorized();
+
+        var newQuery = new LaudoGetByQueryDto
+        {
+            ExameId = query.ExameId,
+            ConsultaId = query.ConsultaId,
+            PacienteId = Guid.Parse(id),
+            MedicoId = query.MedicoId,
+
+            Page = query.Page,
+            Limit = query.Limit,
+            MaxDateCriado = query.MaxDateCriado,
+            MinDateCriado = query.MinDateCriado,
+            Ativo = query.Ativo,
+        };
+
+        var pacientes = await _service.Handler(newQuery);
+        return Ok(pacientes);
+    }
+}
